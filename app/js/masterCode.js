364df0ced9257ac8a395318641861aadd33b83e5
cmigz <cmignacca@gmail.com>
2018-05-26 07:19:08 -0400

    Adds Master Code & masterGenerator
    app/js/masterCode.js =>
    - Initiates masterCode as an empty array
    - Initiates code color options
    - Creates Function: masterGenerator
      - Generates a master code of four random colors based of options
        available from colors arr

--- app/js/masterCode.js
+++ app/js/masterCode.js
@@ -1,1 +1,13 @@
+let masterCode = [];
+const colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple'];
 
+function masterGenerator() {
+  masterCode = [];
+  let i = 0;
+  while(i < 4) {
+    masterCode.push(colors[Math.floor(Math.random() * 6 + 0)]);
+    i += 1;
+  }
+}
+
+export default masterGenerator;
